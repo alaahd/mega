9f57078f2934f5ffd9bed10bb316620782e8acf4
aghoshx <aghoshx@gmail.com>
2013-09-20 00:28:02 +0530

    New entry

--- new_data.sql
+++ new_data.sql
@@ -2,3 +2,4 @@ INSERT INTO `mega`.`options` (`qid`, `choice`, `file_name`) VALUES ('8', 'Footjo
 INSERT INTO `mega`.`options` (`qid`, `choice`, `file_name`) VALUES ('8', 'Greg Norman', 'img/logos/norman.jpg');
 INSERT INTO `mega`.`options` (`qid`, `choice`, `file_name`) VALUES ('9', 'Ping', 'img/logos/ping.jpg');
 INSERT INTO `mega`.`options` (`qid`, `choice`, `file_name`) VALUES ('9', 'Titleist', 'img/logos/titleist.jpg');
+INSERT INTO `mega`.`options` (`qid`, `choice`, `file_name`) VALUES ('10', 'Yas Links', 'img/logos/yas.jpg');
