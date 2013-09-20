816f064546f7208f28d41b084195eacb4c42fae0
Alaa <alaahd@gmail.com>
2013-09-20 12:56:47 +0400

    purge the cache custom.js

--- index.php
+++ index.php
@@ -624,6 +624,6 @@
 		});
 	</script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<script type="text/javascript" src="js/custom.js"></script>
+		<script type="text/javascript" src="js/custom.js?v1"></script>
 </body>
 </html>
