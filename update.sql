c5b84454583a5a36689da5b65f6ad30a2b5b8f6d
aghoshx <aghoshx@gmail.com>
2013-10-02 12:10:19 +0530

    UPDATE.SQL Equipment of the Year

--- update.sql
+++ update.sql
@@ -1,5 +1,12 @@
+/*
 UPDATE  `mega`.`options` SET  `choice` =  'Al Badia Golf Club <span>(18th Hole, Par 5)</span>' WHERE  `options`.`option_id` =14;
 UPDATE  `mega`.`options` SET  `choice` =  'Emirates Golf Club <span>(Majlis course)</span>' WHERE  `options`.`option_id` =7;
 UPDATE  `mega`.`options` SET  `choice` =  'Abu Dhabi HSBC Golf Championship <span>(in association with the Abu Dhabi Tourism and Cultural Authority)</span>' WHERE `options`.`option_id` =107;
 UPDATE  `mega`.`options` SET  `choice` =  'Emirates Golf Club (Majlis Course)' WHERE  `options`.`option_id` =21;
 UPDATE  `mega`.`options` SET  `choice` =  'Emirates Golf Club - Majlis Practice Facility' WHERE  `options`.`option_id` =42;
+*/
+
+INSERT INTO  `mega`.`options` (`qid` ,`choice` ,`file_name`) VALUES ('9',  'Trojan Batteries',  'trojan.jpg');
+INSERT INTO  `mega`.`options` (`qid` ,`choice` ,`file_name`) VALUES ('9',  'Club Car',  'clubcar.jpg');
+INSERT INTO  `mega`.`options` (`qid` ,`choice` ,`file_name`) VALUES ('9',  'Flowtronex',  'flowtronex.jpg');
+INSERT INTO  `mega`.`options` (`qid` ,`choice` ,`file_name`) VALUES ('9',  'Toro',  'toro.jpg');
